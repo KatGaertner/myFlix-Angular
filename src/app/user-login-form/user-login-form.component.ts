8616871c12283dde9c2535ac6da50cb215b39fc2
KatGaertner <97692910+KatGaertner@users.noreply.github.com>
2023-08-27 13:52:30 +0200

    added signup and login forms

---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss'],
+})
+export class UserLoginFormComponent implements OnInit {
+  @Input() userData = { username: '', password: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {}
+
+  loginUser(): void {
+    this.fetchApiData.loginUser(this.userData).subscribe({
+      next: (response) => {
+        // TODO: logic for successful user registration
+        this.dialogRef.close();
+        console.log(response);
+        this.snackBar.open('Successfully logged in!', 'OK', { duration: 3000 });
+      },
+      error: (response) => {
+        console.log(response);
+        this.snackBar.open(response, 'OK', { duration: 3000 });
+      },
+    });
+  }
+}
---
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-registration-form',
+  templateUrl: './user-registration-form.component.html',
+  styleUrls: ['./user-registration-form.component.scss'],
+})
+export class UserRegistrationFormComponent implements OnInit {
+  @Input() userData = { name: '', password: '', email: '', birthday: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+    public snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {}
+
+  registerUser(): void {
+    this.fetchApiData.registerUser(this.userData).subscribe({
+      next: (response) => {
+        // Logic for a successful user registration goes here! (To be implemented)
+        this.dialogRef.close();
+        console.log(response);
+        this.snackBar.open('Successfully signed up!', 'OK', { duration: 3000 });
+      },
+      error: (response) => {
+        console.log(response);
+        this.snackBar.open(response, 'OK', { duration: 3000 });
+      },
+    });
+  }
+}
