8163adca736178dd4f853a861d1e77d26b255725
KatGaertner <97692910+KatGaertner@users.noreply.github.com>
2023-08-29 20:18:59 +0200

    changed page loading

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -22,14 +22,16 @@ export class MainPageComponent implements OnInit {
 
   getMovies(): void {
     const data = localStorage.getItem('movies');
-    if (data) this.movies = JSON.parse(data);
-    else {
+    if (data) {
+      this.movies = JSON.parse(data);
+      this.featured = this.movies.filter((movie) => movie.featured === true);
+    } else {
       this.fetchApiData.getAllMovies().subscribe((resp: any) => {
         this.movies = resp;
         localStorage.setItem('movies', JSON.stringify(resp));
+        this.featured = this.movies.filter((movie) => movie.featured === true);
       });
     }
-    this.featured = this.movies.filter((movie) => movie.featured === true);
   }
 
   logout(): void {
